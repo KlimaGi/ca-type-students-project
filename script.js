0bf410da5bf00507c9708ef9dd6818c8057790ae
Giedre <giedre.klima@gmail.com>
2022-06-22 23:16:15 +0300

    with steps example

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const studentFormEl = document.querySelector("form");
+const studentFormEl = document.querySelector("form#student-form");
+const searchForm = document.querySelector("form#search-form");
+// jei null tai kuriamas studentas
+let editStudent = null;
 
 const INITIAL_STUDENT_DATA = [
   {
@@ -53,32 +56,49 @@ const INITIAL_STUDENT_DATA = [
   },
 ];
 
+let hideByStar = (word) =>
+  word
+    .split("")
+    .map((a) => (a = "*"))
+    .join("");
+
 function renderInitialData(students) {
   students.map((student) => {
+    let studentName = student.name;
+    let studentSurname = student.surname;
+    let studentAge = student.age;
+    let studentPhone = student.phone;
+    let studentEmail = student.email;
+    let studentKnowledge = student.itKnowledge;
+    let studentGroup = student.groupName;
+
+    let interests = student.interests;
+
     let studentsListEl = document.querySelector("#students-list");
     let studentItem = document.createElement("div");
     studentItem.classList.add("student-item");
 
     let nameEl = document.createElement("p");
-    nameEl.innerHTML = `<strong>Name</strong>: ${student.name}`;
+    nameEl.innerHTML = `<strong>Name</strong>:
+    <span class="student-name"> ${studentName} </span>`;
 
     let surnameEl = document.createElement("p");
-    surnameEl.innerHTML = `<strong>Surname</strong>: ${student.surname}`;
+    surnameEl.innerHTML = `<strong>Surname</strong>: <span class="student-surname">${studentSurname}</span>`;
 
     let studentAgeEl = document.createElement("p");
-    studentAgeEl.innerHTML = `<strong>Age</strong>: ${student.age}`;
+    studentAgeEl.innerHTML = `<strong>Age</strong>: ${studentAge}`;
 
     let studentPhoneEl = document.createElement("p");
-    studentPhoneEl.innerHTML = `<strong>Phone</strong>: ${student.phone}`;
+    studentPhoneEl.innerHTML = `<strong>Phone</strong>: ${studentPhone}`;
 
     let studentEmailEl = document.createElement("p");
-    studentEmailEl.innerHTML = `<strong>Email</strong>: ${student.email}`;
+    studentEmailEl.innerHTML = `<strong>Email</strong>: ${studentEmail}`;
 
     let studentKnowledgeEl = document.createElement("p");
-    studentKnowledgeEl.innerHTML = `<strong>IT knowledge</strong>: ${student.itKnowledge}`;
+    studentKnowledgeEl.innerHTML = `<strong>IT knowledge</strong>: ${studentKnowledge}`;
 
     let studentGroupEl = document.createElement("p");
-    studentGroupEl.innerHTML = `<strong>Student group</strong>: ${student.groupName}`;
+    studentGroupEl.innerHTML = `<strong>Student group</strong>: ${studentGroup}`;
     // --------- get all checkboxes checked values
     let interestWrapperEl = document.createElement("div");
     interestWrapperEl.classList.add("interest-wrapper");
@@ -90,7 +110,7 @@ function renderInitialData(students) {
     let interestListEl = document.createElement("ul");
     interestListEl.classList.add("interest-list");
 
-    student.interests.forEach((el) => {
+    interests.forEach((el) => {
       let interestItemElement = document.createElement("li");
       interestItemElement.textContent = el;
 
@@ -98,6 +118,37 @@ function renderInitialData(students) {
     });
 
     interestWrapperEl.append(interestTitleEl, interestListEl);
+
+    // buttons
+    let privateInfoButton = document.createElement("button");
+    privateInfoButton.textContent = "Show private info";
+    privateInfoButton.classList.add("private-btn");
+
+    privateInfoButton.addEventListener("click", () => {
+      if (!privateInfoButton.classList.contains("hide")) {
+        studentPhoneEl.innerHTML = `<strong>Phone</strong>: ${studentPhone}`;
+        studentEmailEl.innerHTML = `<strong>Email</strong>: ${studentEmail}`;
+        privateInfoButton.textContent = "Hide personal info";
+      } else {
+        let hidePhone = hideByStar(studentPhone);
+        let hideEmail = hideByStar(studentEmail);
+        studentPhoneEl.innerHTML = `<strong>Phone</strong>: ${hidePhone}`;
+        studentEmailEl.innerHTML = `<strong>Email</strong>: ${hideEmail}`;
+        privateInfoButton.textContent = "Show personal info";
+      }
+      privateInfoButton.classList.toggle("hide");
+    });
+
+    let deleteStudentButton = document.createElement("button");
+    deleteStudentButton.textContent = "Remove student";
+    deleteStudentButton.classList.add("btn-delete");
+
+    deleteStudentButton.addEventListener("click", () => {
+      console.log(studentName);
+      studentItem.remove();
+      let deleteText = `Student ${studentName} ${studentSurname} is removed`;
+      alertMessage(deleteText);
+    });
 
     studentItem.append(
       nameEl,
@@ -107,7 +158,9 @@ function renderInitialData(students) {
       studentEmailEl,
       studentKnowledgeEl,
       studentGroupEl,
-      interestWrapperEl
+      interestWrapperEl,
+      privateInfoButton,
+      deleteStudentButton
     );
     studentsListEl.prepend(studentItem);
   });
@@ -195,13 +248,8 @@ studentFormEl.addEventListener("submit", (event) => {
       }
     }
   });
-  if (!validForm) return;
 
-  let hideByStar = (word) =>
-    word
-      .split("")
-      .map((a) => (a = "*"))
-      .join("");
+  if (!validForm) return;
 
   let studentsListEl = document.querySelector("#students-list");
   let studentItem = document.createElement("div");
@@ -240,9 +288,9 @@ studentFormEl.addEventListener("submit", (event) => {
   let interestListEl = document.createElement("ul");
   interestListEl.classList.add("interest-list");
 
-  interests.forEach((el) => {
+  interests.forEach((interest) => {
     let interestItemElement = document.createElement("li");
-    interestItemElement.textContent = el.value;
+    interestItemElement.textContent = interest.value;
 
     interestListEl.append(interestItemElement);
   });
@@ -274,10 +322,42 @@ studentFormEl.addEventListener("submit", (event) => {
   deleteStudentButton.classList.add("btn-delete");
 
   deleteStudentButton.addEventListener("click", () => {
-    console.log(studentName);
     studentItem.remove();
     let deleteText = `Student ${studentName} ${studentSurname} is removed`;
     alertMessage(deleteText);
+  });
+
+  let editStudentButton = document.createElement("button");
+  editStudentButton.textContent = "Edit";
+
+  editStudentButton.addEventListener("click", () => {
+    // console.dir(studentFormEl.elements.group.value);
+    //studentFormEl.elements.group.value = "type 2";
+
+    studentFormEl.elements.name.value = studentName;
+    studentFormEl.elements.surname.value = studentSurname;
+    studentFormEl.elements.age.value = studentAge;
+    studentFormEl.elements.phone.value = studentPhone;
+    studentFormEl.elements.email.value = studentEmail;
+    studentFormEl.elements["it-knowledge"].value = studentKnowledge;
+    studentFormEl.elements.group.value = studentGroup;
+
+    studentFormEl.elements.languages.forEach((formInterest) => {
+      formInterest.checked = false;
+
+      interests.forEach((studentInterest) => {
+        if (studentInterest.value === formInterest.value) {
+          formInterest.checked = true;
+        }
+      });
+    });
+
+    studentFormEl.querySelector('[type="submit"]').textContent = "Save Changes";
+
+    // jei ne button, bet input type submit, input neturi text content, nes yra save
+    // uzdarantis tag'as, todel VALUE
+    //studentFormEl.querySelector('[type="submit"]').value = "Save Changes";
+    editStudent = studentItem;
   });
 
   studentItem.append(
@@ -290,16 +370,38 @@ studentFormEl.addEventListener("submit", (event) => {
     studentGroupEl,
     interestWrapperEl,
     privateInfoButton,
-    deleteStudentButton
+    deleteStudentButton,
+    editStudentButton
   );
   studentsListEl.prepend(studentItem);
 
   //studentFormEl.reset();
   event.target.reset();
-
-  let alertText = `Student created ${studentName} ${studentSurname}`;
-  alertMessage(alertText);
 });
+
+// if (editStudent) {
+//   console.log("redaguojamas studentas");
+
+//   // edit saugo originalaus studentItem reiksme
+//   console.log(editStudent);
+//   // studentItem kintamasis saugo dabartine formos studento reiksme
+//   console.log(studentItem);
+
+//   editStudent.replaceWith(studentItem);
+//   editStudent = null;
+
+//   let alertText = `Student edited (${studentName} ${studentSurname})`;
+//   alertMessage(alertText);
+
+//   studentFormEl.querySelector('[type="submit"]').textContent = "Submit";
+// } else {
+//   console.log("kuriamas naujas studentas");
+
+//   // studentListEl.prepend(studentItem);
+
+//   let alertText = `Student created (${studentName} ${studentSurname})`;
+//   alertMessage(alertText);
+// }
 
 function alertMessage(text, elementClass = "") {
   const alertEl = document.querySelector("#alert");
@@ -327,3 +429,25 @@ function inputErrorMessage(inputElement, errorMessage) {
 }
 // document.body.dataset.hide = false;
 // console.log(document.body.dataset.hide);
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let searchInput = event.target.elements.search.value.toLowerCase().trim();
+
+  let allStudents = document.querySelectorAll(".student-item");
+
+  allStudents.forEach((student) => {
+    let studentName = student
+      .querySelector(".student-name")
+      .textContent.toLowerCase()
+      .trim();
+
+    if (studentName.includes(searchInput)) {
+      student.style.display = "block";
+    } else {
+      // student.style.background = "powderblue";
+      student.style.display = "none";
+    }
+  });
+});
